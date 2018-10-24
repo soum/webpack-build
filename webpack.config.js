c472b688201f250b6b0845402a045b6caf2054b1
soum <sghosh@pandora.net>
2018-10-23 20:30:39 -0400

    updated logic to parse module.exports = []

--- webpack.config.js
+++ webpack.config.js
@@ -3,47 +3,44 @@ var pathMap = require('./PathMap');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 
 module.exports = env => {
-	console.log('region' + env.region);
-	switch(env.region){
-		case 'emea':
-			return [{
-				mode: 'development',
-				entry: require('./GetFilePath').getFilePath(),
-				output: {
-					filename: '[name].js',
-					path: path.resolve(__dirname, 'app_dist_'+env.region)
-				},
-				module: pathMap.webpackModule[env.region],
-				plugins: [
-					new SpriteLoaderPlugin()
-				]
-			},{
-				mode: 'development',
-				entry: require('./GetFilePath').getFilePath(),
-				output: {
-					filename: '[name].js',
-					path: path.resolve(__dirname, 'app_dist')
-				},
-				module: pathMap.webpackModule.refapp,
-				plugins: [
-					new SpriteLoaderPlugin()
-				]
-			}]
-		break;
-		default:
-			return {
-				mode: 'development',
-				entry: require('./GetFilePath').getFilePath(),
-				output: {
-					filename: '[name].js',
-					path: path.resolve(__dirname, 'app_dist')
-				},
-				module: pathMap.webpackModule.refapp,
-				plugins: [
-					new SpriteLoaderPlugin()
-				]
-			}
-
+	console.log(env.region, env.instance);
+	if(env.region !== 'refapp'){
+		return [{
+			mode: 'development',
+			entry: require('./GetFilePath').getFilePath(),
+			output: {
+				filename: '[name].js',
+				path: path.resolve(__dirname, 'app_dist_'+env.region)
+			},
+			module: pathMap.webpackModule[env.region],
+			plugins: [
+				new SpriteLoaderPlugin()
+			]
+		},{
+			mode: 'development',
+			entry: require('./GetFilePath').getFilePath(),
+			output: {
+				filename: '[name].js',
+				path: path.resolve(__dirname, 'app_dist')
+			},
+			module: pathMap.webpackModule.refapp,
+			plugins: [
+				new SpriteLoaderPlugin()
+			]
+		}]
+	}else {
+		return {
+			mode: 'development',
+			entry: require('./GetFilePath').getFilePath(),
+			output: {
+				filename: '[name].js',
+				path: path.resolve(__dirname, 'app_dist')
+			},
+			module: pathMap.webpackModule.refapp,
+			plugins: [
+				new SpriteLoaderPlugin()
+			]
+		}
 	}
 }
 
