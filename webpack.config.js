200d390387dd9abf5e2371ee6d9292ea40535ae0
soum <sghosh@pandora.net>
2018-10-23 20:31:54 -0400

    updated config into a function

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,33 @@ var path = require('path');
 var pathMap = require('./PathMap');
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 
-module.exports = env => {
-	console.log(env.region, env.instance);
-	if(env.region !== 'refapp'){
-		return [{
+class RuntimeObject {
+	static returnObject(env, outputPath) {
+		var obj = { 
 			mode: 'development',
 			entry: require('./GetFilePath').getFilePath(),
 			output: {
-				filename: '[name].js',
-				path: path.resolve(__dirname, 'app_dist_'+env.region)
+				filename: '[name].bundle.js',
+				path: path.resolve(__dirname, outputPath)
 			},
 			module: pathMap.webpackModule[env.region],
 			plugins: [
 				new SpriteLoaderPlugin()
 			]
-		},{
-			mode: 'development',
-			entry: require('./GetFilePath').getFilePath(),
-			output: {
-				filename: '[name].js',
-				path: path.resolve(__dirname, 'app_dist')
-			},
-			module: pathMap.webpackModule.refapp,
-			plugins: [
-				new SpriteLoaderPlugin()
-			]
-		}]
+		};
+		return obj;
+	}
+}
+
+module.exports = env => {
+	console.log(env.region);
+	if(env.region !== 'refapp'){
+		return [
+			RuntimeObject.returnObject(env, 'app_dist_'+env.region),
+			RuntimeObject.returnObject(env, 'app_dist')
+		]
 	}else {
-		return {
-			mode: 'development',
-			entry: require('./GetFilePath').getFilePath(),
-			output: {
-				filename: '[name].js',
-				path: path.resolve(__dirname, 'app_dist')
-			},
-			module: pathMap.webpackModule.refapp,
-			plugins: [
-				new SpriteLoaderPlugin()
-			]
-		}
+		return RuntimeObject.returnObject(env, 'app_dist');
 	}
 }
 
